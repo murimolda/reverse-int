84929f0679807fded34f69eb594ceb49bf3653a6
murimolda <murimolda@gmail.com>
2022-01-14 17:07:11 +0300

    fix: delete Math-function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse(n) {
     let stringN = n.toString().split("").reverse().join("");
-    let reverseNumber = parseFloat(stringN) * Math.sign(n);
+    let reverseNumber = parseFloat(stringN);
     return reverseNumber;
 }
