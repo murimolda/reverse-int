7561f1beee04b21b8db0507ae4d431703812d42e
murimolda <35343729+murimolda@users.noreply.github.com>
2022-01-14 15:33:24 +0300

    feat: add function for reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let stringN = String(n);
+    let reverseNumber = stringN.split("").reverse().join("");
+    reverseNumber = Number(reverseNumber);
+    return reverseNumber;
 }
